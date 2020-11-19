95de613689c9d18c21ef5034287ed546ceffc3b9
Alejandro Sior <aho@sior.be>
2020-11-19 22:26:12 +0100

    implemented the stack

---
+++ example.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "stack.h"
+
+#include <stdio.h>
+
+typedef def_stack(int) int_stack;
+
+void pop(int_stack *t) {
+	stpop(*t);
+}
+
+int main() {
+	int_stack t;
+	stinit(t, int);
+
+	stpush(t, 42);
+	stpush(t, 69);
+	printf("%zu   %d\n", t.len, t.data[t.len-1]);
+
+	pop(&t);
+
+	printf("%zu   %d\n", t.len, t.data[t.len-1]);
+
+	for (int i = 0; i < 10; i++) {
+		stpush(t, i);
+	}
+
+	for (int i = t.len - 1; i >= 0; i--) {
+		printf("%d\n", t.data[i]);
+	}
+
+	stfree(t);
+	return 0;
+}
---
+++ stack.h
@@ -0,0 +1,32 @@
+/*
+	stack.h - written by Alejandro Sior, inspired by rxi/vec.
+
+	A very minimalistic stack/dynamic arrays implementation.
+
+	Requires <stdlib.h> preincluded in including files.
+*/
+
+#define def_stack(X) \
+	struct { \
+		size_t mbs; \
+		size_t len; \
+		X *data; \
+		X popcont; \
+	}
+
+#define stinit(X, T) \
+	(X).mbs = sizeof(T); \
+	(X).len = 0; \
+	(X).data = NULL
+
+#define stpush(X, V) \
+	(X).data = realloc((X).data, ((X).len + 1) * (X).mbs); \
+	(X).data[(X).len++] = V
+
+#define stpop(X) \
+	((X).popcont = (X).data[--(X).len], (X).data = realloc((X).data, (X).len * (X).mbs), (X).popcont)
+
+#define stfree(X) \
+	free((X).data); \
+	(X).data = NULL; \
+	(X).len = 0
