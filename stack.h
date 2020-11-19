4704b13d496afc8e5f7c4afeef107d3b815aeaa3
Alejandro Sior <aho@sior.be>
2020-11-19 22:38:42 +0100

    stpush returns pushed value

--- stack.h
+++ stack.h
@@ -20,8 +20,7 @@
 	(X).data = NULL
 
 #define stpush(X, V) \
-	(X).data = realloc((X).data, ((X).len + 1) * (X).mbs); \
-	(X).data[(X).len++] = V
+	((X).data = realloc((X).data, ((X).len + 1) * (X).mbs), (X).data[(X).len++] = V)
 
 #define stpop(X) \
 	((X).popcont = (X).data[--(X).len], (X).data = realloc((X).data, (X).len * (X).mbs), (X).popcont)
