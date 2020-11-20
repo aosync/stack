58b4988b2d8e79e62a50aa0d2d7e68825f4b0f22
Alejandro Sior <aho@sior.be>
2020-11-20 10:35:14 +0100

    expression-ified stinit and stfree

--- stack.h
+++ stack.h
@@ -14,18 +14,22 @@
 		X popcont; \
 	}
 
-#define stinit(X, T) \
-	(X).mbs = sizeof(T); \
-	(X).len = 0; \
-	(X).data = NULL
+#define stinit(X, T) ( \
+	(X).mbs = sizeof(T), \
+	(X).len = 0, \
+	(X).data = NULL)
 
-#define stpush(X, V) \
-	((X).data = realloc((X).data, ((X).len + 1) * (X).mbs), (X).data[(X).len++] = V)
+#define stpush(X, V) ( \
+	(X).data = realloc((X).data, \
+	((X).len + 1) * (X).mbs), \
+	(X).data[(X).len++] = V)
 
-#define stpop(X) \
-	((X).popcont = (X).data[--(X).len], (X).data = realloc((X).data, (X).len * (X).mbs), (X).popcont)
+#define stpop(X) ( \
+	(X).popcont = (X).data[--(X).len], \
+	(X).data = realloc((X).data, (X).len * (X).mbs), \
+	(X).popcont)
 
-#define stfree(X) \
-	free((X).data); \
-	(X).data = NULL; \
-	(X).len = 0
+#define stfree(X) ( \
+	free((X).data), \
+	(X).len = 0, \
+	(X).data = NULL)
